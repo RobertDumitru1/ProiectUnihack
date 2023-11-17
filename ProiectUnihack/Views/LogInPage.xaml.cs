81a3d9ee7b3c7a1e731866408e9e62d9062fdc8d
Ichim_A.George <george_ichim1586@yahoo.com>
2023-11-17 22:01:25 +0200

    login test

--- ProiectUnihack/Views/LogInPage.xaml.cs
+++ ProiectUnihack/Views/LogInPage.xaml.cs
@@ -7,4 +7,21 @@ public partial class LogInPage : ContentPage
 		InitializeComponent();
 	}
 
+    public string UsernameEntry { get; private set; }
+	public string PasswordEntry { get; private set; }
+
+    private void LogIn(object sender, EventArgs e)
+    {
+        string username=UsernameEntry;
+        string password = PasswordEntry;
+        if (IsValidUser(username, password))
+        {
+            DisplayAlert("Loging In", $"Welcome, {username}", "ok");
+        }
+        else DisplayAlert("Not Logging In", "Wrong credentials introducedced", "ok");
+    }
+    private bool IsValidUser(string username, string password)
+    {
+        return username == "user1" && password == "password1";
+    }
 }
