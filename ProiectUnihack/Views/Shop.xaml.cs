38be9387cdcb490b6b376d9e807e899e3df57025
RobertDumitru1 <robert2006h@gmail.com>
2023-11-19 13:51:43 +0200

    Gata e finalul, promit

--- ProiectUnihack/Views/Shop.xaml.cs
+++ ProiectUnihack/Views/Shop.xaml.cs
@@ -6,4 +6,14 @@ public partial class Shop : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ImageButton_Clicked(object sender, EventArgs e)
+    {
+		DisplayAlert("Can't afford", "You dont't have enough banuti", "ok");
+    }
+
+    private void ImageButton_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
 }
